096af148925b0e24f7370672296a354ec97eb3df
Glo18 <kiogoraspecial@gmail.com>
2021-02-22 04:42:23 +0300

    create getter methods in both classes

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,6 +1,46 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hero {
+    private String name;
+    private int age;
+    private String power;
+    private String weaknesses;
+    private static List<Hero> instances = new ArrayList<>();
+
+
     public Hero(String name, int age, String power, String weakness) {
+        this.name = name;
+        this.age = age;
+        this.power = power;
+        this.weaknesses = weaknesses;
+        instances.add(this);
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public int getAge() {
+
+        return age;
+    }
+
+    public String getPower() {
+
+        return power;
+    }
+
+    public String getWeaknesses() {
+
+        return weaknesses;
+    }
+
+    public static List<Hero> getAll() {
+
+        return instances;
     }
 }
