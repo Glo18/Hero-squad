fe16e35a0521416fcf7535473610784b726b71dc
Glo18 <kiogoraspecial@gmail.com>
2021-02-22 04:52:57 +0300

    create root route and add content

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,4 +13,10 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
     }
+
+    get("/", (request, response) -> {
+        Map<String, Object> model = new HashMap<>();
+        return new ModelAndView(model, "index.hbs");
+    }, new HandlebarsTemplateEngine());
+}
 }
