df600bd96b66b2b1b332c79bc37436edfb0f75a7
Glo18 <kiogoraspecial@gmail.com>
2021-02-22 04:20:55 +0300

    create route in app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,16 @@
+//import models.Hero;
+//import models.Squad;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import static spark.Spark.*;
+
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+    }
 }
