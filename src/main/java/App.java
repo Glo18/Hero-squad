ea79bbaac764983b6e61c481b85ae904ecf1591c
Glo18 <kiogoraspecial@gmail.com>
2021-02-22 21:19:44 +0300

    add hero forms,get and post methods

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
-//import models.Hero;
-//import models.Squad;
+import models.Hero;
+import models.Squad;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -18,5 +18,23 @@ public class App {
         Map<String, Object> model = new HashMap<>();
         return new ModelAndView(model, "index.hbs");
     }, new HandlebarsTemplateEngine());
-}
+
+    get("/heroform", (request, response) -> {
+        Map<String, Object> model = new HashMap<>();
+        return new ModelAndView(model, "heroform.hbs");
+    }, new HandlebarsTemplateEngine());
+
+    post("/new/hero", (request, response) -> {
+
+        Map<String, Object> model = new HashMap<>();
+        String name = request.queryParams("name");
+        int age = Integer.parseInt(request.queryParams("age"));
+        String power = request.queryParams("power");
+        String weakness = request.queryParams("weakness");
+        Hero newHero = new Hero(name,age,power,weakness);
+        request.session().attribute("item",name);
+        model.put("item",request.session().attribute("item"));
+        model.put("newHero",newHero);
+        return new ModelAndView(model, "successhero.hbs");
+    }, new HandlebarsTemplateEngine());
 }
